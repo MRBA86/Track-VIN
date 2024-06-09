64372bad0f89b3fd95e021f2811084fe6711e78d
MRBA86 <m.r.b2769973@gmail.com>
2024-06-09 13:35:03 +0330

    Create Home page and Base Template
    Create Home page and Base Template
    config static files in settings.py

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse('This is Home Page')
+    return render(request, 'home/index.html')
 
 # Create your views here.
+
